031f78e35567f1781d4ad14f548783c66f3961cc
Erik <erik.ra@gmail.com>
2017-04-25 13:57:30 +0200

    Add project files.

---
+++ IveBeenEverywhereMan/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IveBeenEverywhereMan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            int t = int.Parse(Console.ReadLine());
+            int[] outputs = new int[t];
+
+            for (int i = 0; i < t; i++)
+            {
+                HashSet<string> inputs = new HashSet<string>();
+
+                int n = int.Parse(Console.ReadLine());
+
+                for (int ii = 0; ii < n; ii++)
+                {
+
+                    inputs.Add(Console.ReadLine());
+                }
+                outputs[i] = inputs.Count;
+            }
+
+            foreach (var i in outputs)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
